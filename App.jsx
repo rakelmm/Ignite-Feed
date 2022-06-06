1d1bbcf885f542c40117849337c9e69c69fcbfca
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-05 22:30:24 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Peter Mendonça"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iusto rem suscipit voluptate doloremque praesentium quisquam illum nesciunt sapiente. Quas in eaque dignissimos animi soluta reprehenderit cupiditate perferendis, fugit doloremque. "
+          />
+          <Post
+            author="Rakel Moreira"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iusto rem suscipit voluptate doloremque praesentium quisquam illum nesciunt sapiente. Quas in eaque dignissimos animi soluta reprehenderit cupiditate perferendis, fugit doloremque. "
+          />
+        </main>
+      </div>
+    </div>
+
+  )
+}
---
+++ components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.modules.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/rakelmm.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Rakel Moreira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='05 de junho às 22:02h' dataTime='2022-06-05 22:02:30'>Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="#">jane.design/doctorcare</a></p>
+                <p><a href="#"> #novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+    )
+}

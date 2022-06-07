10e469949fb223e3b26eadefb46d7962699f4e1c
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-07 15:55:31 -0300

    Componente: Comment

---
+++ components/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from './Comment.module.css';
+
+import { Trash } from 'phosphor-react';
+import { ThumbsUp } from 'phosphor-react';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
+                alt=""
+            />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Devon Lane</strong>
+                            <time title='05 de junho às 22:02h' dataTime='2022-06-05 22:02:30'>Cerca de 2h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Rakel, parabéns!! 👏 👏 </p>
+                </div>
+                <footer>
+                  <button>
+                      <ThumbsUp />
+                      Aplaudir <span>20</span>
+                  </button>
+                </footer>
+            </div>
+        </div>
+    )
+}

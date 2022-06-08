55b9bb631ded81b86609d682fb8ca24943994dd3
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-08 16:01:53 -0300

    Iterando no JSX

--- App.jsx
+++ App.jsx
@@ -5,6 +5,38 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 
 import './global.css';
+import { KeyReturn } from 'phosphor-react';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rakelmm.png",
+      name: 'Rakel Moreira',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-08 15:32:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-01 15:32:00'),
+  },
+];
 
 export function App() {
   return (
@@ -14,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Peter Mendonça"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iusto rem suscipit voluptate doloremque praesentium quisquam illum nesciunt sapiente. Quas in eaque dignissimos animi soluta reprehenderit cupiditate perferendis, fugit doloremque. "
-          />
-          <Post
-            author="Rakel Moreira"
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit iusto rem suscipit voluptate doloremque praesentium quisquam illum nesciunt sapiente. Quas in eaque dignissimos animi soluta reprehenderit cupiditate perferendis, fugit doloremque. "
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>

ba7ce006a7d9975153d2ce29849d414cf593f653
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-08 14:56:13 -0300

    Aplicando Responsividade

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,7 +3,7 @@ import styles from './Avatar.module.css'
 export function Avatar({ hasBorder = true, src }) {
 
     return (
-        <img 
+        <img
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
             src={src}
         />

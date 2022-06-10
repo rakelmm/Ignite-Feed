f084be9ac74eb77f79e34d8315c938fde17ee2d1
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-09 22:11:44 -0300

    Entendendo a Key

--- components/Post.jsx
+++ components/Post.jsx
@@ -7,7 +7,7 @@ import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-// estado = variáveis que eu quero que o componente monitore
+// estado = variáveis que eu quero que o componente monitore 
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
@@ -57,9 +57,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p><a href="#">{line.content}</a></p>;
+                        return <p key={line.content}><a href="#">{line.content}</a></p>;
                     }
                 })}
             </div>
@@ -81,7 +81,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article>
